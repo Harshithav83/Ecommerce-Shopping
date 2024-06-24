aecd96bd1d100e4ea174a13724b328f06674bd2d
Harshitha V <117572809+Harshithav83@users.noreply.github.com>
2024-06-24 21:11:23 +0530

    Add files via upload

---
+++ About.php
@@ -0,0 +1,62 @@
+<?php
+$active = "Contact";
+include('db.php');
+include("functions.php");
+include("header.php");
+
+?>
+
+
+<!-- Breadcrumb Section Begin -->
+<div class="breacrumb-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="breadcrumb-text">
+                    <a href="index.php"><i class="fa fa-home"></i> Home</a>
+                    <span>About Us</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Breadcrumb Section Begin -->
+
+<!-- About Us Section Begin -->
+<section class="About-section spad">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-5">
+                <div class="About-title">
+                    <h4>About Us</h4>
+                    <p>Welcome to Subashitha Collections, your ultimate destination for fashion-trends. We selected the latest trends and timeless classics, ensuring you have access to quality products that reflect your unique style. With a commitment to exceptional customer service and continuous  shopping experiences, we're here to make every moment memorable. Join us as we redefine the art of shopping, one delightful discovery at a time.
+
+Happy Shopping!</p>
+                </div>
+                
+                    
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 offset-lg-1">
+                        
+                            </div>
+                        </form>
+
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Contact Section End -->
+
+<?php
+include('footer.php');
+?>
+
+
+</body>
+
+</html>
